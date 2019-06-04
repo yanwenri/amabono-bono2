20171a4010ba730cff2c1ecfa53894ec9ea1a2f9
yanwenri <50934392+yanwenri@users.noreply.github.com>
2019-06-04 23:46:40 +0900

    Update BonoRestController.java

--- src/main/java/com/sk/amabono/bono/application/sp/web/BonoRestController.java
+++ src/main/java/com/sk/amabono/bono/application/sp/web/BonoRestController.java
@@ -1,5 +1,9 @@
 package com.sk.amabono.bono.application.sp.web;
 
+import java.text.ParseException;
+import java.util.Date;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
@@ -8,7 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.sk.amabono.bono.domain.model.Address;
 import com.sk.amabono.bono.domain.model.Bono;
+import com.sk.amabono.bono.domain.model.BonoState;
+import com.sk.amabono.bono.domain.model.MemMemberlevelType;
 import com.sk.amabono.bono.domain.repository.BonoRepository;
 
 
@@ -22,11 +29,9 @@ public class BonoRestController {
 	@RequestMapping("/login")
 	public String login() { 
 		return "login"; 
-	} 
-	   
+	}
 	
-	
-	
+	  
 	
 	/*
 	 * @RequestMapping(value="/login", method = RequestMethod.POST) public void
@@ -34,11 +39,10 @@ public class BonoRestController {
 	 * 
 	 * bonorepository.save(bono); }
 	 */
-	
 
 	
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public void PostBono(HttpServletRequest request, HttpServletResponse response){
+	public void PostBono(HttpServletRequest request, HttpServletResponse response) throws ParseException{
 			
 	
 		/*
@@ -48,21 +52,47 @@ public class BonoRestController {
     //getParameter의 반환값은 String이다. 
     String name = request.getParameter("name");
     String Desc = request.getParameter("Desc");
-    String address = request.getParameter("address");
-    String requestcount = request.getParameter("requestcount");
-    String requiredlevel = request.getParameter("requiredlevel");
+    String addressd = request.getParameter("address");
+    String zip = request.getParameter("zip");
+    String requestcountd = request.getParameter("requestcount");
+    String requiredleveld = request.getParameter("requiredlevel");
 
-		 
-    //멤버객체에 회원정보를 저장한다.
-    Bono bono = new Bono();
-    bono.setName(name);
-    bono.setDesc(Desc);
-    bono.setAddress(null);
-    bono.setRequestcount(0);
-    bono.setRequiredlevel(null);
+    String date = request.getParameter("day");
+    
+    Date DAY = new java.text.SimpleDateFormat("yyyyMMdd").parse(date);
 
     
+    int zipcode = Integer.parseInt(zip);
+    int requestcount = Integer.parseInt(requestcountd);
+
+    MemMemberlevelType requiredlevel = MemMemberlevelType.valueOf(requiredleveld);
+    BonoState bonostate = BonoState.valueOf("REGIST");
+    
+    Address address = new Address();
+    
+    address.setAddress(addressd);
+    
+    System.out.println(addressd);
+    
+    address.setZipCode(zipcode);
+		 
+    String uniqueKey = UUID.randomUUID().toString();
+    
+    //멤버객체에 회원정보를 저장한다.
+    Bono bono=new Bono();
+    
+    bono.setBono_id(uniqueKey);
+    bono.setName(name);
+    bono.setDesc(Desc);
+    bono.setAddress(address);
+    bono.setRequestcount(requestcount);
+    bono.setRequiredlevel(requiredlevel);
+    bono.setState(bonostate);
+    bono.setDate(DAY);
+    
+    System.out.println(bono.getName());
     System.out.println(bono.getDesc());
+    System.out.println(bono.getAddress().getZipCode());
     
     bonoRepository.save(bono);
 
