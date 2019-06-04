67f8f05b3f8bfd9b340805b7c77d61639fbe8e31
yanwenri <50934392+yanwenri@users.noreply.github.com>
2019-06-04 19:56:21 +0900

    Update BonoRestController.java

--- src/main/java/com/sk/amabono/bono/application/sp/web/BonoRestController.java
+++ src/main/java/com/sk/amabono/bono/application/sp/web/BonoRestController.java
@@ -1,5 +1,6 @@
 package com.sk.amabono.bono.application.sp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,25 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.sk.amabono.bono.domain.model.Bono;
-
+import com.sk.amabono.bono.domain.repository.BonoRepository;
 
 @Controller
 public class BonoRestController {
-/*	
+	@Autowired
+	private BonoRepository bonoRepository;
+	
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
 	}
 	//loginForm
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public void loginPOST(String id) {
+	public String loginPOST(String id) {
 		System.out.println("Submit ID :: " + id);
+		Bono bono1 = new Bono();
+		bono1.setName(id);
+		bonoRepository.save(bono1);
+		return "account";
 	}
-	
-	@RequestMapping(value="/create", method = RequestMethod.POST)
-	public ResponseEntity<Void> PostBono(@RequestBody final Bono bono){
+/*	
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String loginPOST(String id) {
 		
-		return new ResponseEntity<Void>;
+		return "/bono";
 	}
-*/		
+*/	
 }
